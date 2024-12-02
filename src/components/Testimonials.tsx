60b17ad0e64021104774ea1903561c0d33ab0407
Hammad Tariq <hammadtariq65@gmail.com>
2024-12-02 02:59:45 -0600

    updated pictures for testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -14,36 +14,44 @@ const testimonials: Testimonial[] = [
     role: 'Product Owner',
     company: 'Macys',
     content: "Hammad was wonderful to work with and provided critical value to the project for which I was the product manager - a Self Checkout application for Macy's customers. He is thorough, asks the right questions, and works collaboratively with team leads, other developers, and QEs to deliver complex functionality against tight deadlines. Do not hesitate to bring Hammad on board to any project!",
-    avatar: '/placeholder.svg?height=60&width=60',
+    avatar: '/matthew1.webp',
   },
   {
     name: 'Taylor Saven',
     role: 'Engineering Lead',
     company: 'Macys',
     content: 'I worked with Hammad for over a year during the development of self-checkout for Macy’s. He was a key engineer in the success of that project. He established himself as a go-to subject expert in the area of UI design and development. He has a high level of experience in single-page apps, React, various JavaScript frameworks, CSS, and other web technologies. He worked well with all members of the team and became a mentor to others, sharing his knowledge and experience. He was timely and accurate with the delivery of everything he worked on. It was great to partner with him on the project.',
-    avatar: '/placeholder.svg?height=60&width=60',
+    avatar: '/taylor1.webp',
   },
   {
     name: 'Kyle Sebrasky',
     role: 'Sr Cloud Applications Developer',
     company: 'Sherwin-Williams',
     content: 'I have worked with Hammad for about 2 years on 2 separate projects. Hammad continuously drives for results and goes out of his way to research issues. His knowledge in React exceeded our own, and he was able to teach the team the best practices and coding patterns. His ability to troubleshoot and problem-solve is top-notch. Hammad was an asset to my team, and we would not have been as successful without him',
-    avatar: '/placeholder.svg?height=60&width=60',
+    avatar: '/kyle1.webp',
   },
   {
     name: 'Gene Plaks',
-    role: 'Sr. Scrum Master',
+    role: 'Sr. Technical Project Manager',
     company: 'RVO Health',
     content: 'Hammad would make an excellent addition to any software development team. He is a hard worker, an excellent programmer, and, perhaps most importantly, a team player. Hammad is also well-versed in the Scrum methodology and can confidently be relied on by overseas teams. I wholeheartedly recommend him!',
-    avatar: '/placeholder.svg?height=60&width=60',
+    avatar: '/gene1.webp',
+  },
+  {
+    name: 'Kamran Hamid',
+    role: 'Technical Lead',
+    company: 'Prog Stream',
+    content: 'Hammad was instrumental in delivering high-quality solutions on our complex project, showcasing exceptional skills in the JavaScript stack and problem-solving. His proactive approach and effective communication ensured alignment among all stakeholders. He went above and beyond to mentor junior team members and consistently exceeded expectations. Hammad is a reliable and talented engineer who would be a tremendous asset to any team.',
+    avatar: '/kamran1.webp',
   },
   {
     name: 'Zakir Mehmood',
     role: 'Software Architect',
     company: 'VentureDive',
     content: 'Hammad is a dedicated, hardworking, and a valuable resource in the full-stack development. He keeps refining his skills by keeping himself updated with the latest trends. I wish him all the best for his future endeavors.',
-    avatar: '/placeholder.svg?height=60&width=60',
+    avatar: '/zakir1.webp',
   },
+  
 ]
 
 const Testimonials = forwardRef<HTMLElement>((_props, ref) => {
