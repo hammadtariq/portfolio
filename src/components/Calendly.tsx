a5cf0b1c1f92e0ba7f9d5a1d001f7af3c1710162
binpoet <ghawkinst0@gmail.com>
2024-12-09 04:17:11 -0600

    Removed functions for opening and closing calendly modal

--- src/components/Calendly.tsx
+++ src/components/Calendly.tsx
@@ -5,19 +5,11 @@ import { PopupModal } from "react-calendly";
 export default function ScheduleCall() {
   const [showModal, setShowModal] = useState(false);
 
-  const handleButtonClick = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
   return (
     <div>
       <button
         className="bg-white text-blue-600 px-6 py-3 rounded-full font-semibold flex items-center hover:bg-blue-100 transition duration-300"
-        onClick={handleButtonClick}
+        onClick={() => setShowModal(true)}
       >
         <Phone className="mr-1" size={16} />
         Schedule Call
@@ -26,7 +18,7 @@ export default function ScheduleCall() {
         <PopupModal
           url="https://calendly.com/hammadtariq65"
           rootElement={document.getElementById("root") as HTMLElement}
-          onModalClose={handleCloseModal}
+          onModalClose={() => setShowModal(false)}
           open={showModal}
         />
       )}
